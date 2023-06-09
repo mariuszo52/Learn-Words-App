d928e9070a43c461fafc773e1502f38b7040b83c
mariuszo52 <114677617+mariuszo52@users.noreply.github.com>
2023-06-09 09:51:27 +0200

    Update data-dev.sql

--- src/main/resources/db/changelog/data/data-dev.sql
+++ src/main/resources/db/changelog/data/data-dev.sql
@@ -1,19 +1,19 @@
 insert into category (name) values ('Zwierzęta');
 insert into category (name) values ('Dom');
 INSERT INTO word (polish_name, translation, image_link, sentence, priority, category_id)
-VALUES ('Kot', 'Cat', 'https://tiny.pl/cxpp5', 'My cat likes to play with a ball.', 1, 1);
+VALUES ('Kot', 'Cat', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg', 'My cat likes to play with a ball.', 1, 1);
 
 INSERT INTO word (polish_name, translation, image_link, sentence, priority, category_id)
-VALUES ('Pies', 'Dog', 'https://tiny.pl/cxppk', 'I take my dog for a walk every morning.', 1, 1);
+VALUES ('Pies', 'Dog', 'https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQkrjYxSfSHeCEA7hkPy8e2JphDsfFHZVKqx-3t37E4XKr-AT7DML8IwtwY0TnZsUcQ', 'I take my dog for a walk every morning.', 1, 1);
 
 INSERT INTO word (polish_name, translation, image_link, sentence, priority, category_id)
-VALUES ('Samochód', 'Car', 'https://tiny.pl/cxplq', 'I need to buy a new car.', 1, 2);
+VALUES ('Samochód', 'Car', 'https://hips.hearstapps.com/hmg-prod/images/2023-mclaren-artura-101-1655218102.jpg?crop=1.00xw:0.847xh;0,0.153xh&resize=1200:*', 'I need to buy a new car.', 1, 2);
 
 INSERT INTO word (polish_name, translation, image_link, sentence, priority, category_id)
-VALUES ('Książka', 'Book', 'https://tiny.pl/cxplg', 'I enjoy reading a good book in my free time.', 1, 2);
+VALUES ('Książka', 'Book', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg/640px-Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg', 'I enjoy reading a good book in my free time.', 1, 2);
 
 INSERT INTO word (polish_name, translation, image_link, sentence, priority, category_id)
-VALUES ('Telefon', 'Phone', 'https://tiny.pl/cxplw', 'I forgot my phone at home.', 1, 2);
+VALUES ('Telefon', 'Phone', 'https://m.media-amazon.com/images/I/61mTrgSkbQL.jpg', 'I forgot my phone at home.', 1, 2);
 
 
 INSERT INTO user_role (name, description) VALUES ('ADMIN', 'Może robić wszystko');
@@ -48,6 +48,3 @@ insert into language (name) values ('turecki');
 
 UPDATE word set language_id = 1 WHERE language_id is null;
 UPDATE word set user_id = 1 WHERE user_id is null;
-
-
-
